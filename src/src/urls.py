0ba9798c1e22923f9757bf904f952999f2d6c155
sheep333 <yukiko.elike1.51@gmail.com>
2020-07-18 23:56:05 +0900

    URLの変更

--- src/src/urls.py
+++ src/src/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('checker/', include('checker.urls')),
+    path('', include('checker.urls')),
 ]
